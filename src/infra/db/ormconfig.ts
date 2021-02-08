338fee8cef5d8371e305e15eb4e54f8b5b6293f2
chalfel <caiohalcsik@gmail.com>
2021-02-08 19:19:29 -0300

    chore: add github actions

--- src/infra/db/ormconfig.ts
+++ src/infra/db/ormconfig.ts
@@ -2,12 +2,13 @@ import * as dotenv from 'dotenv'
 import path from 'path'
 dotenv.config()
 export default {
-  type: 'postgres',
+  type: process.env.NODE_ENV === 'test' ? 'sqlite' : 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database:
+    process.env.NODE_ENV === 'test' ? ':memory:' : process.env.DB_DATABASE,
   synchronize: false,
   logging: false,
   entities: [path.join(__dirname, '/../../domain/entity/**/*')],
